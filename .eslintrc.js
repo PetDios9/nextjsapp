69e913012979ebf1381ed67eeef305c2f24899bd
Peter Diosdado <diosdadopeter@gmail.com>
2022-01-19 14:08:15 -0800

    eslint airbnb file

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/jsx-filename-extension': 0,
+    'react/react-in-jsx-scope': 0,
+    'react/prop-types': 0,
+    'react/jsx-props-no-spreading': 0,
+  },
+};
